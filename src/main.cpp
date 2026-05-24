9dbd8bbb952818247b784ab40f8a90d3c9fdc2aa
jmzn00 <mansikkajaakko13@gmail.com>
2026-05-24 22:11:54 +0300

    Textures

--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,14 @@
 #include <cmath>
 #include "Shader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 const int SCREEN_WIDTH = 3440;
 const int SCREEN_HEIGHT = 1440;
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
-
-const char *vertexShaderSource = R"(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    layout (location = 1) in vec3 aColor;
-    
-    out vec3 ourColor;
-
-    void main()
-    {
-        gl_Position = vec4(aPos, 1.0);
-        ourColor = aColor;
-    })";
-const char *fragmentShaderSource = R"(
-    #version 330 core
-    out vec4 FragColor;
-    in vec3 ourColor;
-
-    void main()
-    {
-        FragColor = vec4(ourColor, 1.0f);
-    }
-)";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -82,34 +62,69 @@ int main()
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    float firstTriangle[] = 
+    float vertices[] = 
     {
-        // positions        // colors
-        0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
-        0.5f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,
-        0.25f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+        // positions        // colors           // texture coords
+        0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    0.0f, 0.0f,
+       -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f,    0.0f, 1.0f
+    };
+    unsigned int indices [] = 
+    {
+        0, 1, 3,
+        1, 2, 3        
     };
 
     // -- VERTEX BUFFER OBJECT & VERTEX ARRAY OBJECT CREATION & CONFIGURATION
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
 
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
     glBindVertexArray(VAO);    
+
+    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position atrribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
 
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    // -- 
+    
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
-    Shader ourShader("shaders/shader.vs", "shaders/shader.fs");
+    Shader shaderProgram("shaders/shader.vs", "shaders/shader.fs");
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "ERROR::TEXTURE::FAILED_TO_LOAD" << std::endl;        
+    }
+    stbi_image_free(data);
 
     // -- RENDER LOOP
     while (!glfwWindowShouldClose(window))
@@ -119,9 +134,12 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);    
 
-        ourShader.use();
+        shaderProgram.use();
+        shaderProgram.setFloat("xOffset", 0.5f);
+
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();                
